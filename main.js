85a3aaebc619a6628f0f2fa15dce177b325a6a38
Nick Shryock <n.shryock10@gmail.com>
2021-01-18 19:26:34 -0800

    Portfolio Project Plan

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+//Program to generate a random mad lib with different sentences, verbs, nouns, and adjectives 
 //Protfolio Project 1 using Java Script and Node.js
 //Author: Nick Shryock
 
-console.log('Hello!');
+//Create Arrays initialized with nouns, verbs, and adjectives
+
+//Create an object with sentences that have properties for the nuber of nouns, verbs, adjectives required for the sentence
+//Within the message object, have another property for each of the other sentence parameters
+
+//Create function to randomly chose the correct number of each element and string together
+
+//Call the randomMessgaeGenerator function and print to the screen
