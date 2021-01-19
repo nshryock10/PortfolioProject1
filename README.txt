afb8427ce230804069669fc5a9bba89e2f1609ab
Nick Shryock <n.shryock10@gmail.com>
2021-01-19 07:14:29 -0800

    Removes unnecessary code in main.js and finishes README.txt

--- README.txt
+++ README.txt
@@ -1,1 +1,10 @@
-First JS Portfolio Project
+# Random Madlib Generator
+## JavaScript Portfolio Project
+
+Simple program run on command line that generates a random mad lib.
+
+All data saved in main.js in JavaScript
+
+New setneces can be added while placing "*" around the noun, verb, or adjective
+Only include one noun, one verb, and one adjective in each sentence
+Verbs, nouns, or adjectives can also be added in their respective array
--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 //Author: Nick Shryock
 //Start Date: Jan. 18, 2021
 
-//Create an object with sentences that have properties for the nuber of nouns, verbs, adjectives required for the sentence
-//Within the message object, have another property for each of the other sentence parameters
+//Object with sentences, nouns, verbs, and adjectives to use
+//Includes methods to pull random parameters and put together in a sentence
 const messages = {
     sentences: ['Yesterday I ate *noun* while I was *verb*ing in my *adjective* outfit!',
     'It is very *adjective* out today. I think I will *verb* with *noun*.',
@@ -12,51 +12,9 @@ const messages = {
     nouns: ['dog', 'friend', 'banana', 'umbrella', 'tree', 'pizza', 'shoe', 'taco', 'mouse'],
     verbs: ['run', 'bike', 'sing', 'shout', 'crawl', 'walk', 'drive', 'climb', 'hike', 'surf'],
     adjectives: ['funny', 'hyper', 'colorful', 'happy', 'enormous', 'jolly', 'plump', 'mysterious', 'adventurous', 'shaggy'],
-    findIndex(sentence) {
-        //This method will find the index of each noun, verb, and adjective. It will replace the word before finding the the next parameter
-        //Object stores the starting index and the word type
-        const newWord = {nounIndex: 0,
-                verbIndex: 0, 
-                adjecIndex: 0};
-        for(let i = 0; i < sentence.length; i++){
-            if(sentence[i] === '*'){
-                if(sentence.substring((i+1), (i+5)) === 'noun'){
-                    newWord.nounIndex = i;
-                }else if(sentence.substring((i+1), (i+5)) === 'verb'){
-                    newWord.verbIndex = i;
-                }else if(sentence.substring((i+1), (i+10)) === 'adjective'){
-                    newWord.adjecIndex = i;
-                }
-            }
-        }
-        //Returns the new word object
-        return newWord;
-    },
-    replaceWord(sentence, wordObj, word, type){
-        //This method will replace the placeholder with the chosen word
-        
-        //Find the appropriate index start based on the type
-        let sIndex = 0;
-        let eIndex = 0;
-        switch (type){
-            case 'noun':
-                sIndex = wordObj.nounIdex;
-                eIndex = 6;
-                break;
-            case 'verb':
-                sIndex = wordObj.verbIndex;
-                eIndex = 6;
-                break;
-            case 'adjective':
-                sIndex = word.adjecIndex;
-                eIndex = 11;
-                break;
-            default:
-                sIndex = 'Error';
-                break;
-        }
 
-        //Repalce the word in the sentnece using the start and end index from above
+    replaceWord(sentence, word, type){
+        //Repalce the word in the sentence using the replace function
         let newSent = sentence.replace('*' + type + '*', word);
         return newSent;
     },
@@ -70,27 +28,23 @@ const messages = {
         
         //Pull a random sentence
         const sent = this.randomWord('sentences');
-        //Find index of each word in that sentence
-        const wordIndex = this.findIndex(sent);
+
         //Pull a random noun and add to sentence
         const noun = this.randomWord('nouns');
-        let newSent = this.replaceWord(sent, wordIndex, noun, 'noun');
-        console.log(newSent);
+        let newSent = this.replaceWord(sent, noun, 'noun');
+
         //Pull a random verb and add to sentence
         const verb = this.randomWord('verbs');
-        newSent = this.replaceWord(newSent, wordIndex, verb, 'verb');
-        console.log(newSent);
+        newSent = this.replaceWord(newSent, verb, 'verb');
+  
         //Pull a random adjective and add to sentence
         const adjective = this.randomWord('adjectives');
-        newSent = this.replaceWord(newSent, wordIndex, adjective, 'adjective');
+        newSent = this.replaceWord(newSent, adjective, 'adjective');
         
         //Print out the message
         console.log(newSent);
     }
 }
 
-//Create function to randomly chose the correct number of each element and string together
-
 //Call the randomMessgaeGenerator function and print to the screen
-
 messages.generateMessage();
